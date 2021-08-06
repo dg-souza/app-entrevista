88ce2cfeff13aea0f12a708b34aa977af25fc173
Davi <davisouzag@gmail.com>
2021-08-05 23:28:06 -0300

    Criação da lista e modal de cadastro

---
+++ src/Api.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export const http = axios.config({
+    Api : 'https://api.mocki.io/v1/a2790e8c'
+})
---
+++ src/views/Cadastro.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 class='h1'>Tela de Cadastro</h1>
+        <formulario id='cadastro'/>
+    </div>
+</template>
+
+<script>
+import Formulario from '../components/Formulario.vue'
+export default {
+    name: 'Cadastro',
+    components: {
+        'formulario' : Formulario
+    }
+}
+</script>
+
+<style scoped>
+#cadastro {
+    padding-left: 5%;
+    padding-top: 5%;
+}
+</style>
